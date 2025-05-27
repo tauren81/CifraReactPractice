abfc047f4c7d1a6409c452188c7cede52f956c15
tauren81 <tauren-81@mail.ru>
2025-05-27 13:09:16 +0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { defineConfig } from 'vite';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  base: '/CifraReactPractice/',
+  base: './',
   plugins: [react(), viteTsconfigPaths()],
   server: {
     port: 3000,
